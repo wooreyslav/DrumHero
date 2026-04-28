ca4659455c3d0a6c16fa7c3ed1b7fed11d8da4aa
wooreyslav <vlad-samokhvalov95@yandex.ru>
2026-04-28 13:21:05 +0500

    chore: hardcode s3 keys, remove sed injection

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 export const YS3_BUCKET     = 'drumhero-tracks';
 export const YS3_REGION     = 'ru-central1';
 export const YS3_ENDPOINT   = 'https://storage.yandexcloud.net';
-export const YS3_ACCESS_KEY = '__YS3_ACCESS_KEY__';   // ← вставь идентификатор ключа
-export const YS3_SECRET_KEY = '__YS3_SECRET_KEY__';      // ← вставь секретный ключ
+export const YS3_ACCESS_KEY = 'YCAJEoYp06hvcAJ4K92DIv2-v';   // ← вставь идентификатор ключа
+export const YS3_SECRET_KEY = 'YCPEvJ0gWqtK011Pj45mhphDkx6iWu21POSP1ZdP';      // ← вставь секретный ключ
